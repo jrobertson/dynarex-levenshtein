70e77ede0d5fd5dec0e7d01841629dd30485a615
Your Name <james@r0bertson.co.uk>
2012-04-10 21:37:57 +0100

    bug fix: the last increment for dynarex record creation was modified

---
+++ dynarex-levenshtein.gemspec
@@ -0,0 +1,8 @@
+Gem::Specification.new do |s|
+  s.name = 'dynarex-levenshtein'
+  s.version = '0.1.2'
+  s.summary = 'dynarex-levenshtein'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/**/*.rb']
+  s.add_dependency('dynarex')
+end
---
+++ lib/dynarexlevenshtein.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+
+# file: dynarexlevenshtein.rb
+
+require 'dynarex'
+
+class DynarexLevenshtein
+
+  attr_reader :to_dynarex, :distance
+
+  def initialize(a,b)
+    @distance = levenshtein(a,b)
+  end
+
+  def levenshtein(a,b)
+
+    a, b = [a, b].sort_by(&:length)
+    n, m = a.length, b.length
+
+    schema = 'table[title,string1,string2]/rows(' + (['line','label'] + (1..n)\
+      .map{|x| "col%s" % x}).join(',') + ')'
+    @to_dynarex = Dynarex.new(schema)
+
+    @to_dynarex.summary[:title] = 'Calculates the Levenshtein distance between a and b.'
+    @to_dynarex.summary[:string1] =  a
+    @to_dynarex.summary[:string2] =  b
+
+    current = (n+1).times.to_a
+
+    top_label = ['0','',''] + a.split('')    
+    label = [''] + b.split('')
+    h = Hash[([:line, :label] + (0..n).map{|x| ("col%s" % x).to_sym})\
+      .zip(top_label)]
+    @to_dynarex.create h
+
+    (1..m).each do |i|
+      previous, current = current, [i]+[0]*n
+
+      h = Hash[([:line, :label] + (0..n).map{|x| ("col%s" % x).to_sym}).zip([i.to_s, \
+        label[i-1]] + previous)]
+      @to_dynarex.create h
+
+      (1..n).each do |j|
+        delete, change, add = current[j-1], previous[j-1], previous[j]
+        change += 1 if a[j-1] != b[i-1]
+        current[j] = [add+1, delete+1, change].min
+      end
+    end
+
+    h = Hash[([:line, :label] + (0..n).map{|x| ("col%s" % x).to_sym}).zip([(m+1).to_s, label[-1]] + current)]
+    @to_dynarex.create h
+
+    current[n]
+  end
+end
