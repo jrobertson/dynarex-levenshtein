8271a2ce83d89ff7cfa16ff8bd71db0e2a16844a
Your Name <james@r0bertson.co.uk>
2012-04-10 21:48:51 +0100

    the distance is now returned in the Dynarex summary

--- lib/dynarexlevenshtein.rb
+++ lib/dynarexlevenshtein.rb
@@ -17,8 +17,8 @@ class DynarexLevenshtein
     a, b = [a, b].sort_by(&:length)
     n, m = a.length, b.length
 
-    schema = 'table[title,string1,string2]/rows(' + (['line','label'] + (1..n)\
-      .map{|x| "col%s" % x}).join(',') + ')'
+    schema = 'table[title,string1,string2, distance]/rows(' + \
+        (['line','label'] + (1..n).map{|x| "col%s" % x}).join(',') + ')'
     @to_dynarex = Dynarex.new(schema)
 
     @to_dynarex.summary[:title] = 'Calculates the Levenshtein distance between a and b.'
@@ -50,6 +50,7 @@ class DynarexLevenshtein
     h = Hash[([:line, :label] + (0..n).map{|x| ("col%s" % x).to_sym}).zip([(m+1).to_s, label[-1]] + current)]
     @to_dynarex.create h
 
+    @to_dynarex.summary[:distance] = current[n]
     current[n]
   end
 end
